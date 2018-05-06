cc0a2da314b229fd757f2f703b6f47dd089111f0
Charlie Jackson <charlie@charliejackson.com>
2018-05-06 11:33:19 +0100

    Get the app config file

--- src/native/start.js
+++ src/native/start.js
@@ -1,1 +1,10 @@
-module.exports = () => console.log("start");
+const path = require("path");
+
+module.exports = () => {
+  console.log("start");
+  const appPath = path.join(process.cwd(), "app.json");
+
+  const appConfig = require(appPath);
+
+  console.log(appConfig);
+};
