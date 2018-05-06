758e7d83c33b1bb71bd76c24ce37dd7de86faed3
Charlie Jackson <charlie@charliejackson.com>
2018-05-06 11:30:22 +0100

    Added a dummy start command

--- src/native/index.js
+++ src/native/index.js
@@ -1,16 +1,14 @@
 const program = require("commander");
+const start = require("./start");
 
 program
   .command("native <cmd>")
-  .description("execute the given remote cmd")
-  .option("-e, --exec_mode <mode>", "Which exec mode to use")
+  .description("Execute a command with the native template")
   .action(function(cmd, options) {
-    console.log('exec "%s" using %s mode', cmd, options.exec_mode);
-  })
-  .on("--help", function() {
-    console.log("  Examples:");
-    console.log();
-    console.log("    $ deploy exec sequential");
-    console.log("    $ deploy exec async");
-    console.log();
+    switch (cmd) {
+      case "start":
+        return start();
+      default:
+        console.log("Unknown command");
+    }
   });
