baa9b6d28a402b74585c0574ae053f3f5b3d01a1
Charlie Jackson <charlie@charliejackson.com>
2018-05-07 10:01:51 +0100

    Web template just renders the src component for now

--- src/web/template/src/App.js
+++ src/web/template/src/App.js
@@ -1,19 +1,5 @@
-import React, { Component } from "react";
 import config from "project-templates/project.config";
 
 const ProjectRoot = config.entryComponent;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <ProjectRoot />
-      </div>
-    );
-  }
-}
-
-export default App;
+export default ProjectRoot;
