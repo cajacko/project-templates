07fed9e43b211e31f00e1c4dd0a03dc2f45be3d7
Charlie Jackson <charlie@charliejackson.com>
2018-05-08 15:44:02 +0100

    Fail when promise rejects

--- src/web/index.js
+++ src/web/index.js
@@ -12,7 +12,12 @@ program
   .description("Execute a command with the web template")
   .action(function(cmd, options) {
     if (commands[cmd]) {
-      return linkProject().then(() => commands[cmd](cmd, options));
+      linkProject()
+        .then(() => commands[cmd](cmd, options))
+        .catch(e => {
+          console.error(e);
+          process.exit(1);
+        });
     }
 
     console.log("Unknown command");
