9e5c93bf336f202fe8816b6788d66239f23808fe
Charlie Jackson <charlie@charliejackson.com>
2018-05-10 15:06:49 +0100

    Auto deploy on commit

---
+++ scripts/deploy.js
@@ -0,0 +1,27 @@
+const latestVersion = require("latest-version");
+const semver = require("semver");
+const path = require("path");
+const fs = require("fs-extra");
+const runCommand = require("../src/utils/runCommand");
+const pkg = require("../package.json");
+
+const pkgVersion = pkg.version;
+
+latestVersion("@cajacko/project-templates").then(latestVersion => {
+  const versionToUse = semver.gt(pkgVersion, latestVersion)
+    ? pkgVersion
+    : semver.inc(latestVersion, "patch");
+
+  //   return runCommand(path.join(__dirname, "../"), "yarn", [
+  //     "publish",
+  //     "--access=public",
+  //     "--new-version",
+  //     versionToUse
+  //   ]);
+
+  const contents = Object.assign({}, pkg, { version: versionToUse });
+
+  return fs.writeJson(path.join(__dirname, "../package.json"), contents, {
+    spaces: 2
+  });
+});
